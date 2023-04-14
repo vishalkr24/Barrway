47796ea9f9a12c845ce6019fa2c417460cd2ef64
shoyeb17 <deepak.gkp888@gmail.com>
2023-04-14 18:58:22 +0530

    Changes EOD by Rohan

---
+++ Barrway.DTO/Common/RoleMasterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barrway.DTO.Common
+{
+    public class RoleMasterModel
+    {
+        public int Id { get; set; }
+        public string ROLE_NAME { get; set; }
+    }
+}
