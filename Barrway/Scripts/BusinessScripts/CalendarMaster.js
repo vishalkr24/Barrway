fd812914a9870dff492344da2b9cebdef710eb36
shoyeb17 <deepak.gkp888@gmail.com>
2023-05-03 19:06:58 +0530

    Change by rohan

---
+++ Barrway/Scripts/BusinessScripts/CalendarMaster.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    setCalendarMaster();
+})
+
+function setCalendarMaster() {
+    var data = GetCompanyCalendars(localStorage.getItem("COMPANY_ID"), "", "");
+
+    if (data.data != null) {
+        var calendarData = data.data;
+        console.log(calendarData);
+        for (var i = 0; i < calendarData.length; i++) {
+            $("#CalendarMasterTable tbody").append(`<tr>
+            <td>${calendarData[i].COMPANY_CODE}</td>
+            <td>${localStorage.getItem("COMPANY_NAME_ENGLISH")}</td>
+            <td>${calendarData[i].CALENDAR_NAME}</td>
+            <td></td>
+<td></td>
+<td></td>
+            </tr>`);
+        }
+
+    }
+
+}
