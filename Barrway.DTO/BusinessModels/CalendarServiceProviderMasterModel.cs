cf62dbf9a97f800d68d8bacfe536679ca5d5c920
shoyeb17 <deepak.gkp888@gmail.com>
2024-01-03 11:52:14 +0530

    changes by Rohan

---
+++ Barrway.DTO/BusinessModels/CalendarServiceProviderMasterModel.cs
@@ -0,0 +1,24 @@
+﻿using Barrway.DTO.CustomValidations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Barrway.DTO.BusinessModels
+{
+    public class CalendarServiceProviderMasterModel
+    {
+        public string Id { get; set; }
+        public string RESOURCE_CODE { get; set; }
+        public string CALENDAR_CODE { get; set; }
+        public string CALENDAR_NAME { get; set; }
+        public string FIRST_NAME { get; set; }
+        public string MIDDLE_NAME { get; set; }
+        public string LAST_NAME { get; set; }
+        public string STATUS { get; set; } = "ACTIVE";
+    }
+    
+}
