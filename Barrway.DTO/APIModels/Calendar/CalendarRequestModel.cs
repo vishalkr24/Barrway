8fd8af4dcbcdb5be5b49414b035f3e38eb1b6496
Rohan Chaurasia <130461786+RohanChaurasia25@users.noreply.github.com>
2024-07-04 15:43:47 +0530

    Revert "Rohan 04072024"

--- Barrway.DTO/APIModels/Calendar/CalendarRequestModel.cs
+++ Barrway.DTO/APIModels/Calendar/CalendarRequestModel.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.SqlServer.Server;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Barrway.DTO.APIModels.Calendar
@@ -15,20 +14,12 @@ namespace Barrway.DTO.APIModels.Calendar
         //public int ActivityFormId { get; set; }
         public string COMPANY_CODE { get; set; }
         public string CALENDAR_CODE { get; set; }
-        [Required(ErrorMessage = "start date time required")]
+        [Required (ErrorMessage ="start date time required")]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
-        public DateTime start { get; set; }
+        public DateTime start{ get; set; }
         [Required(ErrorMessage = "end date time required")]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
-        public DateTime end { get; set; }
-
-        public List<CalendarAPIFilterModel> calendarFilters { get; set; }
-    }
-
-    public class CalendarAPIFilterModel
-    {
-        public int FilterType { get; set; }
-        public string FilterValue { get; set; }
+        public DateTime end{ get; set; }
     }
 
     public class CalendarEvents
@@ -49,7 +40,7 @@ namespace Barrway.DTO.APIModels.Calendar
 
         public string COMPANY_CODE { get; set; }
         public string CALENDAR_CODE { get; set; }
-        public string start { get; set; }
+        public string start { get; set; }      
         public string end { get; set; }
 
     }
@@ -58,11 +49,11 @@ namespace Barrway.DTO.APIModels.Calendar
 
     public class FavoriteCalendarViewModel
     {
-
+     
         public string COMPANY_CODE { get; set; }
-
+  
         public string CALENDAR_CODE { get; set; }
-
+     
 
     }
 
